21bb20ef4e9517a3dcf21954933e3823c8071a11
RuslanLomaka <wfptny@gmail.com>
2024-11-25 15:44:20 +0100

    11.25.2024 goIt javaCore module 5 task 9 working

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,27 +1,42 @@
-class Engine{
+class Engine {
     private int power;
-
-
-    public static void main(String[] args) {
-        System.out.println(new Engine().getFuelType());
-        System.out.println(new XFuelEngine().getFuelType());
-    }
-
-    public int getPower() {
+    public int getPower () {
         return power;
     }
 
-    public void setPower(int power) {
+    public void setPower ( int power){
         this.power = power;
     }
 
-    public String getFuelType(){
+    public String getFuelType () {
         return "A500";
     }
 }
-class XFuelEngine extends Engine{
+
+class XFuelEngine extends Engine {
+    protected String serialNumber;
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
     @Override
-    public String getFuelType(){
+    public String getFuelType() {
         return "XFuel";
     }
 }
+
+class AdvancedXFuelEngine extends XFuelEngine {
+
+    public void printInfo() {
+        System.out.println("Serial number is " + this.serialNumber + ", power is " + this.getPower());
+    }
+
+    public static void main(String[] args) {
+        AdvancedXFuelEngine engine = new AdvancedXFuelEngine();
+        engine.setPower(1000);
+        engine.setSerialNumber("SN504030");
+        engine.printInfo(); //Serial number is SN504030, power is 1000
+    }
+}
+
