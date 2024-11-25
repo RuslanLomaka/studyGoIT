2e81745306cb5647cd5b778c1894d549e02a32ea
RuslanLomaka <wfptny@gmail.com>
2024-11-25 17:16:12 +0100

    goIt javaCore module 5 task 16 working

---
+++ src/Planets.java
@@ -0,0 +1,27 @@
+public  class Planets {
+    public static final String MERCURY = "Mercury";
+    public static final String VENUS = "Venus";
+    public static final String EARTH = "Earth";
+    public static final String MARS = "Mars";
+    public static final String JUPITER = "Jupiter";
+    public static final String SATURN = "Saturn";
+    public static final String URANUS = "Uranus";
+    public static final String NEPTUNE = "Neptune";
+
+
+        public static void main (String[] args) {
+            Planets Planets = new Planets();
+            System.out.println("The planets are:");
+            System.out.println(Planets.MERCURY);
+            System.out.println(Planets.VENUS);
+            System.out.println(Planets.EARTH);
+            System.out.println(Planets.MARS);
+            System.out.println(Planets.JUPITER);
+            System.out.println(Planets.SATURN);
+            System.out.println(Planets.URANUS);
+            System.out.println(Planets.NEPTUNE);
+
+
+}
+
+}
