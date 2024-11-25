549c8229b2768b9d7c45a8bb97170597d6a92543
RuslanLomaka <wfptny@gmail.com>
2024-11-25 13:05:32 +0100

    0.3

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,5 +1,24 @@
-public class SpaceShip
-{
- public String name ="name";
+public class SpaceShip {
+ private String name;
 
+ public String getName() {
+  return name;
+ }
+
+ public void setName(String name) {
+  this.name = name;
+ }
+
+ //Test output
+ public static void main(String[] args) {
+  SpaceShip ship = new SpaceShip();
+  ship.setName("Walker");
+  System.out.println(ship.getName()); //Should be Walker
+
+  ship.setName("");
+  System.out.println(ship.getName()); //Should be Walker, empty value ignored
+
+  ship.setName("Voyager ".repeat(100));
+  System.out.println(ship.getName()); //Should be Walker, too long value ignored
+ }
 }
