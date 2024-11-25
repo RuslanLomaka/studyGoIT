87b310f22bedfe96e731460ed100693dfe2b05a6
RuslanLomaka <wfptny@gmail.com>
2024-11-25 16:02:22 +0100

    goIt javaCore module 5 task 10 working

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,42 +1,36 @@
-class Engine {
-    private int power;
-    public int getPower () {
-        return power;
-    }
+class Tanker{
+    private String serialNumber;
 
-    public void setPower ( int power){
-        this.power = power;
+    public String getSerialNumber() {
+        return serialNumber;
     }
-
-    public String getFuelType () {
-        return "A500";
-    }
-}
-
-class XFuelEngine extends Engine {
-    protected String serialNumber;
 
     public void setSerialNumber(String serialNumber) {
         this.serialNumber = serialNumber;
     }
-
-    @Override
-    public String getFuelType() {
-        return "XFuel";
+    protected String getFuelType(){
+        return "A500";
     }
-}
-
-class AdvancedXFuelEngine extends XFuelEngine {
-
-    public void printInfo() {
-        System.out.println("Serial number is " + this.serialNumber + ", power is " + this.getPower());
+    public void refuel(int amount){
+        System.out.println("Add "+amount+" of "+this.getFuelType()+", tanker serial number is "+this.serialNumber);
     }
 
     public static void main(String[] args) {
-        AdvancedXFuelEngine engine = new AdvancedXFuelEngine();
-        engine.setPower(1000);
-        engine.setSerialNumber("SN504030");
-        engine.printInfo(); //Serial number is SN504030, power is 1000
+        Tanker tanker = new Tanker();
+        tanker.setSerialNumber("SN504030");
+        tanker.refuel(300);
+    }
+
+}
+class XFuelTanker extends Tanker{
+    @Override
+    protected String getFuelType(){
+        return "XFuel";
     }
 }
-
+class MX200Tanker extends Tanker{
+    @Override
+    protected String getFuelType(){
+        return "MX200";
+    }
+}
