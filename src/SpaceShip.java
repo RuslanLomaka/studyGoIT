de125c7d0b3fbeda33aebe75c501ad5b106ca237
RuslanLomaka <wfptny@gmail.com>
2024-11-25 15:08:30 +0100

    11.25.2024 goIt javaCore module 5 task 7 working

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,61 +1,20 @@
-class SpaceRocketLauncher{
- private int bigRocketCount;//кількість великих ракет
- private int smallRocketCount;//кількість маленьких ракет
+class Engine{
+    private int power;
 
 
-
-    public int getBigRocketCount() {
-        return bigRocketCount;
+    public static void main(String[] args) {
+        System.out.println(new Engine().getFuelType());
     }
 
-    public void setBigRocketCount(int bigRocketCount) {
-       if( 0>bigRocketCount||bigRocketCount>100 ){return;}
-       this.bigRocketCount = bigRocketCount;
+    public int getPower() {
+        return power;
     }
 
-    public int getSmallRocketCount() {
-        return smallRocketCount;
+    public void setPower(int power) {
+        this.power = power;
     }
 
-    public void setSmallRocketCount(int smallRocketCount) {
-     if( 0>smallRocketCount||smallRocketCount>100 ){return;}
-        this.smallRocketCount = smallRocketCount;
+    public String getFuelType(){
+        return "A500";
     }
-
- public int getTotalPower(){/* Цей метод рахує загальну вогневу міць ракетної установки з розрахунку, що кожна велика  ракета, що залишилася, дає 100 одиниць міці, кожна маленька ракета - 50 одиниць міці.*/
-
-     return this.bigRocketCount*100+this.smallRocketCount*50;
- }
-
-    public void launchBigRocket() {
-        if(this.getBigRocketCount()==0) return;
-        System.out.println("Launch big rocket");
-        --this.bigRocketCount;
-    }
-    public void launchSmallRocket() {
-        if(this.getSmallRocketCount()==0) return;
-        System.out.println("Launch small rocket");
-        --this.smallRocketCount;
-    }
-
- public static void main(String[] args) {
-     SpaceRocketLauncher launcher = new SpaceRocketLauncher();
-     launcher.setBigRocketCount(5);
-     launcher.setSmallRocketCount(10);
-
-     System.out.println("Power is " + launcher.getTotalPower());
-
-     launcher.launchBigRocket();
-     System.out.println("Big rocket count is " + launcher.getBigRocketCount());
-
-     launcher.launchSmallRocket();
-     System.out.println("Small rocket count is " + launcher.getSmallRocketCount());
-
-     System.out.println("Power is " + launcher.getTotalPower());
-
- }
-
-
-
-
 }
