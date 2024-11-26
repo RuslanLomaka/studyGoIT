f25f0da1dee3135bb2f64ba61c517521fc15d5bf
RuslanLomaka <wfptny@gmail.com>
2024-11-27 00:11:50 +0100

    goIt javaCore module 3 task 10 working

--- src/HarekCity.java
+++ src/HarekCity.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class HarekCity {
     public String[] createEmptyNameArray() {
         String[] result = new String[0];
@@ -11,10 +12,19 @@ public class HarekCity {
         names[3] = toReplace[1];
     }
     public String firstAndLastTogether(String[] names) {
-       return (names[0] + " AND " + names[9] + " TOGETHER");
+        return names[0] + " AND " + names[9] + " TOGETHER";
+    }
+    public void changeElectResult(String[] results) {
+        String temp = results[0];
+        results[0] = results[results.length - 1];
+        results[results.length - 1] = temp;
+    }
+    public String[] changeElectResultAgain(String[] results) {
+        return new String[]{results[2],results[3],results[4]};
     }
     public static void main(String[] args) {
-        String[] names = new String[]{"io", "pu", "re", "max", "kok", "mis", "hit", "kio", "pax", "rew"};
-        System.out.println(new HarekCity().firstAndLastTogether(names));
+        String[] results = new String[]{"mix", "max", "pex", "fux", "rox"};
+        String[] changedResults = new HarekCity().changeElectResultAgain(results);
+        System.out.println(Arrays.toString(changedResults));
     }
 }
