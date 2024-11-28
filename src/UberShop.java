bbc03e607d42cc58c18ca1b741c2143a8523825c
ruslan <wfptny@gmail.com>
2024-11-28 19:42:31 +0100

    GoIT JavaCore module 4 task 2/27 working (put old files in new dir) united loop

--- src/UberShop.java
+++ src/UberShop.java
@@ -16,13 +16,44 @@ public class UberShop {
         }
     }
 
+    public int[] findMinMaxPrices(int[] prices){
+        if(prices.length==0) return new int[0];
+
+        int maxPrice= Integer.MIN_VALUE;
+        int minPrice= Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price > maxPrice) {
+                maxPrice = price;
+            }
+            if (price < minPrice) {
+                minPrice = price;
+            }
+        }
+
+        if(maxPrice==minPrice) return new int[]{minPrice};
+
+        return new int[]{minPrice,maxPrice};
+    }
+
     //Test output
     public static void main(String[] args) {
         UberShop shop = new UberShop();
 
-        //Should be [200, 2250]
-        float[] prices = new float[] {100f, 1500f};
-        shop.multiplyPrices(prices);
-        System.out.println(Arrays.toString(prices));
+        //Should be [50, 1500]
+        int[] prices = new int[] {100, 1500, 300, 50};
+        int[] prices1= new int[] {10, 50, 3, 1550};
+        int[] prices2= new int[] {50,50};
+        int[] prices3= new int[] {};
+
+        int[] minMax = shop.findMinMaxPrices(prices);
+        int[] minMax1 = shop.findMinMaxPrices(prices1);
+        int[] minMax2 = shop.findMinMaxPrices(prices2);
+        int[] minMax3 = shop.findMinMaxPrices(prices3);
+
+        System.out.println(Arrays.toString(minMax));
+        System.out.println(Arrays.toString(minMax1));
+        System.out.println(Arrays.toString(minMax2));
+        System.out.println(Arrays.toString(minMax3));
     }
 }
