6e0fa0564240655c732417d63bbe344efc3bc563
RuslanLomaka <wfptny@gmail.com>
2024-11-28 02:22:04 +0100

    goIt javaCore module 3 task 26/27 working

---
+++ src/CaptainDisputeAgain.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class CaptainDisputeAgain {
+    public int findMin(int[] triple){
+        return Integer.min(Integer.min(triple[0],triple[1]), triple[2]);
+    }
+
+
+    public static void main(String[] args) {
+        CaptainDisputeAgain CaptainDisputeAgain = new CaptainDisputeAgain();
+
+        System.out.println(CaptainDisputeAgain.findMin(new int[] {1, 10, 3}));
+        System.out.println(CaptainDisputeAgain.findMin(new int[] {50, 4, 100}));
+    }
+
+}
