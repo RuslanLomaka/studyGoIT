1ef39c2f98915ad63de7d74aab5d5a821b1f7d5d
RuslanLomaka <wfptny@gmail.com>
2024-12-01 14:01:32 +0100

    goIt javaCore module 5 task 24/39 working

---
+++ src/Stars.java
@@ -0,0 +1,58 @@
+    public class Stars {
+
+        private int count;
+        public int getCount() {
+            return count;
+        }
+        public void setCount(int count) {
+            this.count = count;
+        }
+
+
+        @Override
+        public String toString() {
+            int[] starDigits = {1000, 100, 10, 1};
+            int remainder = count;
+            StringBuilder starNumber = new StringBuilder();
+
+            for(int digit: starDigits){
+                int currentDigitCount=remainder/digit;
+                for(int i=currentDigitCount;i>0;i--){
+                    switch (digit){
+                        case 1000: {
+                            starNumber.append('X');
+                           break;
+                        }
+                        case 100: {
+                            starNumber.append('Y');
+                           break;
+                        }
+                        case 10: {
+                            starNumber.append('Z');
+                           break;
+                        }
+                        case 1:{
+                            starNumber.append('*');
+                           break;
+                        }
+                        default: {
+                            starNumber.append("");
+                            System.out.println("Digit not found");
+                           break;
+                        }
+                    }
+                    remainder=remainder-digit;
+
+                }
+
+            }
+            return starNumber.toString();
+        }
+
+        public static void main(String[] args) {
+            Stars stars = new Stars();
+            stars.setCount(36279);
+            System.out.println(stars);
+            System.out.println(stars.getCount());
+        }
+    }
