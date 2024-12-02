21ecf13bf155a69153ead69c3f10ac99be1914e7
RuslanLomaka <wfptny@gmail.com>
2024-12-02 22:57:56 +0100

    goIt javaCore module 4 task 11/29 working

---
+++ src/RectangleTest.java
@@ -0,0 +1,29 @@
+class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(10, 5);
+        Rectangle r2 = new Rectangle(3, 7);
+        Rectangle r3 = new Rectangle(10, 15);
+
+        //false
+        System.out.println(r1.canPlaceInto(r2));
+
+        //true
+        System.out.println(r1.canPlaceInto(r3));
+
+        //true
+        System.out.println(r2.canPlaceInto(r3));
+
+        //false
+        System.out.println(r3.canPlaceInto(r2));
+    }
+}
+
+class Rectangle{
+    public Rectangle(int a, int b){
+
+    }
+
+    public boolean canPlaceInto(Rectangle anotherRect){
+        return true;
+    }
+}
