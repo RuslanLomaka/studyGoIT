24c1cf88f763de7bb704f02341d3d00fcefcda15
RuslanLomaka <wfptny@gmail.com>
2024-12-02 22:14:42 +0100

    goIt javaCore module 4 task 11/29 working

---
+++ src/RectangleAreaTest.java
@@ -0,0 +1,26 @@
+class RectangleAreaTest {
+    public static void main(String[] args) {
+        int[] coords = {2, 2, 12, 12};
+
+        //Expect 100
+        System.out.println(new RectangleArea(coords).getArea());
+    }
+}
+
+class RectangleArea{
+    int xa;
+    int ya;
+    int xb;
+    int yb;
+
+    public int getArea(){
+        return Integer.signum((xb-xa)*(yb-ya))*(xb-xa)*(yb-ya);
+    }
+
+    public RectangleArea(int[] coords){
+        xa = coords[0];
+        ya = coords[1];
+        xb = coords[2];
+        yb = coords[3];
+    }
+}
